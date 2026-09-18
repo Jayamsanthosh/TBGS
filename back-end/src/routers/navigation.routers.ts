602835f8e73a19332b8c66bf85a78d622b810e4d
erp_support <erpsupport@visioninfotech.co.tz>
2026-09-18 18:00:26 +0300

    Remove all files from branch

--- back-end/src/routers/navigation.routers.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getNavigationHierarchy } from "../controllers/navigation.controller";
-
-const router = Router();
-
-router.get("/", getNavigationHierarchy);
-
-export default router;
